337888bf0965ab92a863fc54616228e9025f5078
lujanespinoza <dev.lujanespinoza@pm.me>
2026-05-28 11:58:26 +0100

    Getters y setters

--- src/net/salesianoscuesta/Investigador/Investigador.java
+++ src/net/salesianoscuesta/Investigador/Investigador.java
@@ -12,4 +12,28 @@ public abstract class Investigador {
         this.sueldo = sueldo;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
 }

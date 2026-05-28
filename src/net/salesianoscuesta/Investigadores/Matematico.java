29e284969d64a926f8c459b01e0e83f07f8d9e9e
lujanespinoza <dev.lujanespinoza@pm.me>
2026-05-28 12:10:18 +0100

    Metodo trabajar Matematico (multiplicar y sumar matrices)

--- src/net/salesianoscuesta/Investigadores/Matematico.java
+++ src/net/salesianoscuesta/Investigadores/Matematico.java
@@ -19,4 +19,41 @@ public class Matematico extends Investigador {
         this.matriz = matriz;
     }
 
+    // Sin parametros solo muestra la matriz actual
+    @Override
+    public String trabajar() {
+        return mostrarMatriz();
+    }
+
+    // Multiplica todos los valores de la matriz por el numero
+    public String trabajar(int numero) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = matriz[i][j] * numero;
+            }
+        }
+        return mostrarMatriz();
+    }
+
+    // Suma la matriz actual con otra matriz 3x3
+    public String trabajar(int[][] otra) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = matriz[i][j] + otra[i][j];
+            }
+        }
+        return mostrarMatriz();
+    }
+
+    private String mostrarMatriz() {
+        String texto = "";
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                texto += matriz[i][j] + " ";
+            }
+            texto += "\n";
+        }
+        return texto;
+    }
+
 }

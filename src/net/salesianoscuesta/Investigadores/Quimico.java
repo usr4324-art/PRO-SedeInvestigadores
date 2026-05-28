2aa3dd7dfba2625ceb95f89bddc8731ddf8c5bf0
lujanespinoza <dev.lujanespinoza@pm.me>
2026-05-28 12:21:43 +0100

    Clase Quimico + ArrayList elementos

--- src/net/salesianoscuesta/Investigadores/Quimico.java
+++ src/net/salesianoscuesta/Investigadores/Quimico.java
@@ -1,5 +1,20 @@
 package net.salesianoscuesta.Investigadores;
 
-public class Quimico {
+import java.util.ArrayList;
+
+import net.salesianoscuesta.Investigador.Investigador;
+
+public class Quimico extends Investigador {
+
+    private ArrayList<String> elementos;
+
+    public Quimico(String nombre, String especialidad, double sueldo) {
+        super(nombre, especialidad, sueldo);
+        this.elementos = new ArrayList<>();
+    }
+
+    public ArrayList<String> getElementos() {
+        return elementos;
+    }
 
 }

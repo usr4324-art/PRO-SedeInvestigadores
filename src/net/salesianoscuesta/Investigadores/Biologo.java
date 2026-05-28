a98dc1b937fda96dfa8b3c71dbf9ba8f16c1b32f
lujanespinoza <dev.lujanespinoza@pm.me>
2026-05-28 12:13:17 +0100

    Clase Biologo + ArrayList especimenes

--- src/net/salesianoscuesta/Investigadores/Biologo.java
+++ src/net/salesianoscuesta/Investigadores/Biologo.java
@@ -1,5 +1,20 @@
 package net.salesianoscuesta.Investigadores;
 
-public class Biologo {
+import java.util.ArrayList;
+
+import net.salesianoscuesta.Investigador.Investigador;
+
+public class Biologo extends Investigador {
+
+    private ArrayList<String> especimenes;
+
+    public Biologo(String nombre, String especialidad, double sueldo) {
+        super(nombre, especialidad, sueldo);
+        this.especimenes = new ArrayList<>();
+    }
+
+    public ArrayList<String> getEspecimenes() {
+        return especimenes;
+    }
 
 }

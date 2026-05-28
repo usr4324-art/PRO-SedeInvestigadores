570b967928e27b39a2abc0f75a3fa8eb97fb8962
lujanespinoza <dev.lujanespinoza@pm.me>
2026-05-28 12:09:16 +0100

    Clase Matematico + atributo matriz

--- src/net/salesianoscuesta/Investigadores/Matematico.java
+++ src/net/salesianoscuesta/Investigadores/Matematico.java
@@ -1,5 +1,22 @@
 package net.salesianoscuesta.Investigadores;
 
-public class Matematico {
+import net.salesianoscuesta.Investigador.Investigador;
+
+public class Matematico extends Investigador {
+
+    private int[][] matriz;
+
+    public Matematico(String nombre, String especialidad, double sueldo, int[][] matriz) {
+        super(nombre, especialidad, sueldo);
+        this.matriz = matriz;
+    }
+
+    public int[][] getMatriz() {
+        return matriz;
+    }
+
+    public void setMatriz(int[][] matriz) {
+        this.matriz = matriz;
+    }
 
 }
